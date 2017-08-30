3e69f66fa5608f38147843d0f5ae88fc12adbc94
xxx <xxx@xxx.com>
2017-08-30 17:37:50 +0800

    提交

--- src/main/java/com/demo01/Demo01.java
+++ src/main/java/com/demo01/Demo01.java
@@ -3,6 +3,7 @@ package com.demo01;
 public class Demo01 {
     public static void main(String[] args){
         System.err.println("Hello world");
-
+        System.err.println("Hello world");
+        System.err.println("Hello world");
     }
 }
