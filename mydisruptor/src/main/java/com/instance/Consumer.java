adda6e602c69cf9485af5d632f271bafb2e760db
20013649 <20013649@suning.com>
2021-03-10 14:10:49 +0800

    `知识点整理汇总： 2020-03-10 `

---
+++ mydisruptor/src/main/java/com/instance/Consumer.java
@@ -0,0 +1,12 @@
+package com.instance;
+
+import com.lmax.disruptor.WorkHandler;
+
+public class Consumer implements WorkHandler<PCData> {
+
+    @Override
+    public void onEvent(PCData event) throws Exception {
+        System.out.println(Thread.currentThread().getId() + "Event: -- " +
+                event.get() * event.get() + "--");
+    }
+}
---
+++ mydisruptor/src/main/java/com/instance/PCDataFactory.java
@@ -0,0 +1,11 @@
+package com.instance;
+
+import com.lmax.disruptor.EventFactory;
+
+public class PCDataFactory implements EventFactory<PCData> {
+
+    @Override
+    public PCData newInstance() {
+        return new PCData();
+    }
+}
