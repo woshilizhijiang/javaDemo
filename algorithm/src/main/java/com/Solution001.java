7125851eae29cb00185d45cb87d8f583b05a4971
20013649 <20013649@suning.com>
2021-04-30 17:51:22 +0800

    `知识点整理汇总： 算法 1.只出现一次数字 2.两数之和 2021-04-30 `

--- algorithm/src/main/java/com/Solution001.java
+++ algorithm/src/main/java/com/Solution001.java
@@ -24,15 +24,15 @@ import java.util.Map;
  */
 public class Solution001 {
     public static void main(String[] args) {
-        int[] nums = {0,1,0,1,0,1,99,99,99,12};
+        int[] nums = {12,0,1,0,1,0,1,99,77,99,77,77,99};
         Solution001 solution001 = new Solution001();
-        System.out.println(solution001.singleNumber(nums));
+        System.out.println(solution001.singleNumber2(nums));
     }
 
     /**
      * 传统map
-     * 1.空间复杂度高
-     * 2.时间复杂度O(n)
+     * 1.空间复杂度高：
+     * 2.时间复杂度O(n) 线性
      * @param nums
      * @return
      */
@@ -53,5 +53,28 @@ public class Solution001 {
             }
         }
         return rst;
+    }
+
+    /**
+     * 参考热评 牛皮的解法
+     * 0 ^ x = x
+     * x ^ x = 0
+     * x & ~x = 0
+     * x & ~0 =x
+     * 那么就是很好解释上面的代码了。一开始a = 0, b = 0;
+     * x第一次出现后，a = (a ^ x) & ~b的结果为 a = x, b = (b ^ x) & ~a的结果为此时因为a = x了，所以b = 0。
+     * x第二次出现：a = (a ^ x) & ~b, a = (x ^ x) & ~0, a = 0; b = (b ^ x) & ~a 化简， b = (0 ^ x) & ~0 ,b = x;
+     * x第三次出现：a = (a ^ x) & ~b， a = (0 ^ x) & ~x ,a = 0; b = (b ^ x) & ~a 化简， b = (x ^ x) & ~0 , b = 0;所以出现三次同一个数，a和b最终都变回了0.
+     * 只出现一次的数，按照上面x第一次出现的规律可知a = x, b = 0;因此最后返回a.
+     * @param nums
+     * @return
+     */
+    public int singleNumber2(int[] nums){
+        int a=0,b=0;
+        for (int num : nums) {
+            a = (a ^ num) & ~b;
+            b = (b ^ num) & ~a;
+        }
+        return a;
     }
 }
